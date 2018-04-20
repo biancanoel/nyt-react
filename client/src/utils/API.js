e673a2396f62361fa396952d2920b2fc2355410a
Bianca Torres <biancatorres@Biancas-MacBook.local>
2018-04-20 09:54:01 -0700

    new yarn build

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     getArticles : function (query) {
-        return axios.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&page=1&sort=newest&fq="+query+"&api-key=d51d3d36cad340b0a24d832c52ab06d6")
+        return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&page=1&sort=newest&fq="+query+"&api-key=d51d3d36cad340b0a24d832c52ab06d6")
     },
 
     saveArticle: function (articleData) {
